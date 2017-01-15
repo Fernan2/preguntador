714d9ec4e455c86e9e9630d649694dd8345af6f6
Fernando Calatayud <fernandorankia@gmail.com>
2017-01-15 22:49:04 +0100

    Ask the question explicitly

--- preguntador.rb
+++ preguntador.rb
@@ -3,6 +3,8 @@ REFACTORING = {
   2 => 'inline method'
 }
 
+puts '¿Cuáles son los refactors que describe Martin Fowler en su libro "Refactoring"?'
+
 REFACTORING.each do |orden, nombre|
   print "#{orden}: "
   respuesta = gets.to_s.strip.downcase
